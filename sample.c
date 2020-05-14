52b6edab857caab67f5945b717dc7ea3cfd176e1
guro <guro.st66@gmail.com>
2020-05-14 14:12:30 +0900

    add branch_name() in sample.c

--- sample.c
+++ sample.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
+
 int main(){
     printf("Gitコマンド一覧\n");
     printf("git init : リポジトリを作成\n");
     printf("git status : Gitの状態を確認\n");
-    printf("git add : ファイルのステージ\n");
-    printf("git commit : コミットする\n");
+    printf("git add : ファイルをステージ\n");
+    branch_name();
     return 0;
 }
