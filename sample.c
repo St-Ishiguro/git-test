dcbc072771dd7256e88026f22a062a62026d5d23
guro <guro.st66@gmail.com>
2020-05-14 14:42:53 +0900

    add description of clone

--- sample.c
+++ sample.c
@@ -4,10 +4,10 @@ int main(){
     printf("Gitコマンド一覧\n");
     printf("git init : リポジトリを作成\n");
     printf("git status : Gitの状態を確認\n");
-    printf("git add : ファイルのステージ\n");
+    printf("git add : ファイルをステージ\n");
     printf("git commit : コミットする\n");
     printf("git log : コミット履歴を見る\n");
-    printf("git add : ファイルをステージ\n");
+    printf("git clone : クローンをつくる\n");
     branch_name();
     return 0;
 }
